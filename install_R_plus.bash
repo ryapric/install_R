7bdd94c1f8ddeb05a0b2bcdfb45993cd12af9c97
Ryan Price <ryapric@gmail.com>
2018-01-02 11:44:42 -0600

    Better commenting & line breaks
    Also added a line to add the sudo user to the "staff" group, so that any
    R packages later installed will also install to the same shared library folder.

--- install_R_plus.bash
+++ install_R_plus.bash
@@ -1,22 +1,24 @@
 #! /bin/bash
-# Bash shell script that will install R, RStudio Server, their various development tools,
-# and the main Linux packages that the tidyverse needs to have installed.
+# Bash shell script that will install R, RStudio Server, their various
+# development tools, and the main Linux packages that the tidyverse needs
+# to have installed.
 # Then, install the tidyverse itself, and other R packages.
 
-# First, you have to add the most current CRAN repository to Linux, since the official
-# version via (at least) Ubuntu is out-of-date.
-# The exact repo will change based on your particular distro.
+# First, you have to add the most current CRAN repository to Linux, since
+# the official version via (at least) Ubuntu is out-of-date.
+# (The exact repo may change based on your particular distro, if not Ubuntu)
 echo "deb https://cran.rstudio.com/bin/linux/ubuntu $(lsb_release -cs)/" | tee -a /etc/apt/sources.list
 
-# Then, if using Ubuntu (and maybe other distros), you need to add the public key of the
-# CRAN repository, so it's trusted by the OS.
-# Based on instructions from: https://cran.r-project.org/bin/linux/ubuntu/README
+# Then, if using Ubuntu (and maybe other distros), you need to add the
+# public key of the CRAN repository, so it's trusted by the OS.
+# Based on instructions from:
+# https://cran.r-project.org/bin/linux/ubuntu/README
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 
 # Now, update the repository list to reflect the new source
 apt-get update
 
-# Now install the Linux packages (maybe a little overkill, here)
+# Now, install the Linux packages (you can comment out those you
 apt-get install -y \
 	r-base \
 	r-base-dev \
@@ -24,16 +26,29 @@ apt-get install -y \
 	r-cran-littler \
 	libxml2-dev \
 	libcurl4-openssl-dev \
-	libssl-dev \
-	libsecret-1-dev \
-	libmariadb-client-lgpl-dev
-	
-# Remove dependencies
+	libssl-dev
+# (the following  may be commented out as desired)
+# 	libsecret-1-dev \
+# 	libmariadb-client-lgpl-dev
+
+# Remove dependency-only programs that were just installed
 apt-get autoremove -y
 
 # Install the R packages (this will take about half an hour by itself)
 Rscript -e 'install.packages(c("tidyverse", "data.table", "openxlsx", "writexl", "RcppRoll", "devtools", "roxygen2", "testthat", "validate"), dependencies = TRUE, repos = "https://cran.rstudio.com/")'
-# Optional: Install .deb package installer, and install RStudio Server
-apt-get install gdebi-core
-wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
-gdebi rstudio-server-1.1.383-amd64.deb
+
+# OPTIONAL:
+# Add user who ran this script to "staff" group, so all R library installs
+# are in one place in the future
+usermod -aG staff ${SUDO_USER}
+
+# OPTIONAL:
+# Install .deb package installer, and install RStudio Server
+# apt-get install gdebi-core
+# wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
+# gdebi rstudio-server-1.1.383-amd64.deb
+
+# OPTIONAL:
+# Reboot (if for no other reason than to make sure the group changes
+# take effect immediately
+reboot now
